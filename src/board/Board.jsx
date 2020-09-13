547e8921fc1d6e2d39155d341d707a56c0bd634b
Baptiste Doucerain <doucerain.b@gmail.com>
2020-09-13 14:55:57 +0200

    Roughly add square

--- src/board/Board.jsx
+++ src/board/Board.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './Board.css'
+import Square from '../square/Square'
 
 function Board() {
-  return <div className="App_board"></div>
+  return (
+    <div className="App_board">
+      {/* Row 1 */}
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+
+      {/* Row 2 */}
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+
+      {/* Row 3 */}
+      <Square></Square>
+      <Square></Square>
+      <Square></Square>
+    </div>
+  )
 }
 
 export default Board
---
+++ src/square/Square.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Square.css'
+
+function Square() {
+  return <div className="board_square">square</div>
+}
+
+export default Square
