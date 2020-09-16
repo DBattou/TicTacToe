da9663a9353a7447aa62cb5a57eb560825ec2b0c
Baptiste Doucerain <doucerain.b@gmail.com>
2020-09-16 10:19:24 +0200

    Remove dashed border, Fix Board styles

--- src/App.js
+++ src/App.js
@@ -10,14 +10,7 @@ import CurrentPlayer from './components/currentPlayer/CurrentPlayer'
 function App() {
   return (
     <div style={{ padding: '40px' }}>
-      <Grid
-        container
-        spacing={2}
-        style={{ border: '5px dotted green' }}
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
+      <Grid container spacing={2} direction="row" justify="center" alignItems="center">
         <Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
           <CurrentPlayer></CurrentPlayer>
         </Grid>
