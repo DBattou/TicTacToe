81346b3409f706bf23f35975227cb15bb90c3fd8
Baptiste Doucerain <doucerain.b@gmail.com>
2020-09-22 12:51:56 +0200

    Calculate draw each time a moove is made, reset button now gives initial state

--- src/redux/reducer.test.js
+++ src/redux/reducer.test.js
@@ -1,5 +1,11 @@
 import tictactoe, { initialState } from './reducer'
-import { markSquare, resetGame, changeCurrentPlayer, changeScore } from './actions'
+import {
+  markSquare,
+  resetGame,
+  changeCurrentPlayer,
+  changeScore,
+  resetBoard,
+} from './actions'
 
 describe('Reducer', () => {
   test('MARK_SQUARE action ', () => {
@@ -12,6 +18,28 @@ describe('Reducer', () => {
                           '', '', '', ]
 
     expect(tictactoe(state, action)).toEqual({ ...initialState, gameState: newGameState })
+  })
+
+  test('MARK_SQUARE action and there is a draw', () => {
+    const action = markSquare(0)
+
+    // prettier-ignore
+    const gameState = ['', 'O', 'X', 
+                       'X', 'O', 'X', 
+                       'O', 'X', 'O', ]
+
+    const state = { ...initialState, gameState: gameState }
+
+    // prettier-ignore
+    const newGameState = ['X', 'O', 'X', 
+                          'X', 'O', 'X', 
+                          'O', 'X', 'O', ]
+
+    expect(tictactoe(state, action)).toEqual({
+      ...initialState,
+      gameState: newGameState,
+      draw: true,
+    })
   })
 
   test('RESET_GAME action ', () => {
@@ -38,5 +66,16 @@ describe('Reducer', () => {
     const action = changeScore('O')
 
     expect(tictactoe(state, action)).toEqual({ ...initialState, score: { O: 1, X: 0 } })
+  })
+
+  test('ResetBoard action ', () => {
+    const state = { ...initialState, draw: true, winner: 'O' }
+    const action = resetBoard()
+
+    expect(tictactoe(state, action)).toEqual({
+      ...state,
+      draw: false,
+      winner: '',
+    })
   })
 })
