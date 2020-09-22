7b2b7d5faebd15fa6646033b39ad4eae93c26db5
Baptiste Doucerain <doucerain.b@gmail.com>
2020-09-22 12:50:47 +0200

    Create Overlay draw component

---
+++ src/components/overlays/winOverlay.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { resetBoard } from '../../redux/actions'
+import { connect } from 'react-redux'
+import './overlay.css'
+
+function WinOverlay({ resetBoard, winner, currentPlayer }) {
+  return winner ? (
+    <div className="App__overlay" data-testid="winingOverlay">
+      <div className="Overlay__text">
+        <span role="img" aria-label="player">
+          {currentPlayer === 'X' ? '⭕️' : '❌'}
+        </span>
+        <p>Gagne ! </p>
+        <button type="reset" onClick={resetBoard}>
+          OK
+        </button>
+      </div>
+    </div>
+  ) : null
+}
+
+// State
+const mapStateToProps = (state) => {
+  return {
+    winner: state.winner,
+    currentPlayer: state.currentPlayer,
+  }
+}
+
+// Actions
+const mapDispatchToProps = (dispatch) => {
+  return {
+    resetBoard: () => {
+      dispatch(resetBoard())
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(WinOverlay)
